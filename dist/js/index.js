e670ab42d9b4b92a12efb48c10f6753be4734020
Md. Mahmudul Hasan Sadi <sadicoding@gmail.com>
2024-10-31 15:46:33 +0600

    Added

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,23 +6,32 @@ const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
-let showMenu = false; //
+let showMenu = false;
+
+// Toggle menu on button click
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
+    // Open menu actions
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuBranding.classList.add("show");
     menuNav.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
-    showMenu = true; // Set the state to true
+    showMenu = true;
   } else {
+    // Close menu actions with slight delay for smooth animation
     menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuBranding.classList.remove("show");
-    menuNav.classList.remove("show");
     navItems.forEach((item) => item.classList.remove("show"));
-    showMenu = false; // Set the state to false
+    
+    // Delay hiding of menu and its components to allow animations to complete
+    setTimeout(() => {
+      menu.classList.remove("show");
+      menuBranding.classList.remove("show");
+      menuNav.classList.remove("show");
+    }, 500); // Match delay with CSS transition time
+    
+    showMenu = false;
   }
 }
